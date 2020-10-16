31965c2ee9e5396006d54a4a9637c0c8329e3fc0
Elmerrrrrr <volgers80@gmail.com>
2020-10-16 15:40:59 +0200

    update 16-10-2020

--- JavaScript/Eindopdracht/PortFolio/Members.js
+++ JavaScript/Eindopdracht/PortFolio/Members.js
@@ -1,22 +1,66 @@
+let tableBody = document.getElementsByClassName('divTableBody')[0];
 
+let firstNameInput = document.getElementById('fname');
+let lastNameInput = document.getElementById('lname');
+let ageInput = document.getElementById('age');
+let companyInput = document.getElementById('company');
+let usernameInput = document.getElementById('username');
+let emailInput = document.getElementById('email');
+let adressInput = document.getElementById('adress');
+let cityInput = document.getElementById('city');
+let postalCodeInput = document.getElementById('pstcode');
+let countryInput = document.getElementById('country');
 
 function enterData(){
 
-        let firstName = document.getElementById('fname').value;
-        let lastName = document.getElementById('lname').value;
-        let age = document.getElementById('age').value;
-        let company = document.getElementById('company').value;
-        let email = document.getElementById('email').value;
-        let adress = document.getElementById('adress').value;
-        let city = document.getElementById('city').value;
-        let postalCode = document.getElementById('pstcode').value;
-        let country = document.getElementById('country').value;
+    firstName = firstNameInput.value;
+    lastName = lastNameInput.value;
+    age = ageInput.value;
+    company = companyInput.value;
+    username = usernameInput.value;
+    email = emailInput.value;
+    adress = adressInput.value;
+    city = cityInput.value;
+    postalCode = postalCodeInput.value;
+    country = countryInput.value;
 
-        if(!firstName || !lastName || !email){
-            alert('Please fill in all the required fields')
-        }
-        // console.log(firstName);
+    let newDivTableRow = `<div class="divTableRow">
+                        <div class="divTableCell">${firstName}</div>
+                        <div class="divTableCell">${lastName}</div>
+                        <div class="divTableCell">${age}</div>
+                        <div class="divTableCell">${company}</div>
+                        <div class="divTableCell">${username}</div>
+                        <div class="divTableCell">${email}</div>
+                        <div class="divTableCell">${adress}</div>
+                        <div class="divTableCell">${city}</div>
+                        <div class="divTableCell">${postalCode}</div>
+                        <div class="divTableCell">${country}</div>
+                    </div>`;
+
+
+ 
+    if(!firstName || !lastName || !email){
+        firstNameInput.style.backgroundColor = 'Tomato'
+        lastNameInput.style.backgroundColor = 'Tomato'
+        emailInput.style.backgroundColor = 'Tomato'
+        alert('Please fill in all the required fields: \n \n First name, Surname, Email')
+        
+    } else{
+        tableBody.innerHTML += newDivTableRow;
+
+    }
+
 
 }
 
-console.log(.document.getElementsByClassName('divTableCell'));
+
+function deleteRow(){
+
+    if (tableBody.childElementCount >2) {
+
+        tableBody.removeChild(tableBody.lastChild);
+        
+    }
+}
+
+

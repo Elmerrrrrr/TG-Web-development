8fbb7913a62d04800a79687309dab7d76ed6a840
Elmerrrrrr <69678218+Elmerrrrrr@users.noreply.github.com>
2020-12-14 18:44:03 +0100

    Update WeatherExtApi.java

--- SpringBoot/weatherApi/src/main/java/com/weather/api/feignclient/WeatherExtApi.java
+++ SpringBoot/weatherApi/src/main/java/com/weather/api/feignclient/WeatherExtApi.java
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 import java.util.Map;
 //https://jsonplaceholder.typicode.com
-//https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,minutely,daily&appid=fa94c80da214e29fa2248f2642c34fb5
-//https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid=fa94c80da214e29fa2248f2642c34fb5
+//https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,minutely,daily&appid=
+//https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid=
 
 
-@FeignClient(url="https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,minutely,daily&appid=fa94c80da214e29fa2248f2642c34fb5",name = "WeatherApiClient")
+@FeignClient(url="https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,minutely,daily&appid=",name = "WeatherApiClient")
 public interface WeatherExtApi {
 
 @GetMapping("")
